3417b41575b4c43f02220440a55d22c200caa0b1
sisisin <azsisisin@gmail.com>
2015-12-27 13:47:04 +0900

    first commit cs.

---
+++ cs/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tddbc;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var class1 = new Class1();
+			Assert.AreEqual(class1.Hello(), "hello!");
+		}
+	}
+}
